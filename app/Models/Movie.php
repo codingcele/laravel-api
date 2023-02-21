e8781a608e570a87ca3405744ca78a85a10d03cf
codingcele <marcocelegato9@gmail.com>
2023-02-21 17:05:50 +0100

    models collegati

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+        'name',
+        'year',
+        'cashOut',
+
+    ];
+
+    public function genre() {
+
+        return $this -> belongsTo(Genre::class);
+
+    }
+
+    public function tags() {
+
+        return $this -> belongsToMany(Tag::class);
+
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+        'name',
+        'description',
+    ];
+
+    public function movies() {
+
+        return $this -> belongsToMany(Movie::class);
+
+    }
 }
