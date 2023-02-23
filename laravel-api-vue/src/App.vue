82123b0c307ba2f570d49e3a628d84e1afaceb55
codingcele <marcocelegato9@gmail.com>
2023-02-23 16:10:47 +0100

    console log movies

---
+++ laravel-api-vue/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+
+import axios from 'axios';
+
+export default {
+
+  mounted() {
+
+    axios.get('http://localhost:8000/api/v1/movie/all')
+      .then(res => {
+
+        const data = res.data;
+        const success = data.success;
+        const movies = data.response;
+
+        console.log(movies);
+
+      })
+      .catch(err => console.error(err));
+
+  }
+
+};
+</script>
+
+<template></template>
+
+<style></style>
