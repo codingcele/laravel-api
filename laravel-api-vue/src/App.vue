b8b16d7246fac83e3da71c25d1a6b4c35086f8b5
codingcele <marcocelegato9@gmail.com>
2023-02-23 16:53:12 +0100

    vue lista movies a schermo

--- laravel-api-vue/src/App.vue
+++ laravel-api-vue/src/App.vue
@@ -2,27 +2,69 @@
 
 import axios from 'axios';
 
+const API_URL = 'http://localhost:8000/api/v1/';
+
 export default {
 
+  data() {
+
+    return {
+      force_update: false,
+      movies_bk: [],
+      movies: [],
+      genres: [],
+      tags: [],
+      movie_name: '',
+      movie_year: '',
+      movie_cashOut: '',
+      movie_genre: '',
+      movie_tags: [],
+      movie_creating: false,
+      movie_updating: -1,
+
+      test: true
+    }
+  },
+
+  methods: {
+
+    updateMovies() {
+      axios.get(API_URL + 'movie/all')
+        .then(res => {
+
+          const data = res.data;
+          const success = data.success;
+          const movies = data.response;
+
+          if (success) {
+            this.movies = movies;
+          }
+        })
+        .catch(err => console.error(err));
+    }
+
+  },
+
   mounted() {
-
-    axios.get('http://localhost:8000/api/v1/movie/all')
-      .then(res => {
-
-        const data = res.data;
-        const success = data.success;
-        const movies = data.response;
-
-        console.log(movies);
-
-      })
-      .catch(err => console.error(err));
-
+    this.updateMovies();
   }
 
 };
 </script>
 
-<template></template>
+<template>
+  <div>
+    <h1>
+      Movies:
+    </h1>
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        {{ movie.name }}
+        <br>
+        <a href="">UPDATE</a> - <a href="">DELETE</a>
+      </li>
+    </ul>
+  </div>
+</template>
 
 <style></style>
